93575ad1e600249961906fa7c93dc0d98dc5d45e
user08lasya <lasyapriya0807@gmail.com>
2024-11-27 20:53:24 +0530

    tkinter

---
+++ activity1.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from PIL import Image, ImageTk
+root = Tk()
+root.title('image')
+root.geometry('400x400')
+upload = Image.open("street.jpg")
+image = ImageTk.PhotoImage(upload)
+label = Label(root, image=image, height=350, width=300)
+label.place(x=50, y=0)
+label2 = Label(root, text="This is how you add image in Tkinter Window")
+label2.place(x=40, y=360)
+root.mainloop()
---
+++ activity3.py
@@ -0,0 +1,14 @@
+from tkinter import *
+root = Tk()
+root.geometry("400x300")
+root.title("main")
+def topwin():
+    top = Toplevel()
+    top.geometry("180x100")
+    top.title("toplevel")
+    l2 = Label(top, text = "This is a toplevel window")
+    l2.pack()
+    top.mainloop()
+l = Label(root, text = "This is a root window")
+btn = Button(root, text = "Click here to open another window,", command = topwin)
+root.mainloop()
